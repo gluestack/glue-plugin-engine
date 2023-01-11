45a98ae378223983c47b7f4100d26b16b1c22cc0
Faiz A. Farooqui <faizz.af@gmail.com>
2023-01-11 11:10:58 +0530

    chore(v0.0.16): main router + events fixed and crons added

---
+++ src/helpers/add-main-cron.ts
@@ -0,0 +1,29 @@
+import { join } from "path";
+import { writeFile } from "./write-file";
+import { fileExists } from "./file-exists";
+import { createFolder } from "./create-folder";
+import { PluginInstance } from "../PluginInstance";
+
+const construct = async (path: string): Promise<void> => {
+  await createFolder(path);
+};
+
+const checkCreate = async (installationPath: string, fileName: string, fileContent: string): Promise<void> => {
+  const path: string = join(installationPath, '..', 'events', fileName);
+
+  const appExist: boolean = await fileExists(path);
+  if (!appExist) {
+    await construct(path);
+  }
+
+  await writeFile(path, fileContent);
+}
+
+export async function addMainCron(engineInstance: PluginInstance): Promise<void> {
+  const installationPath = engineInstance.getInstallationPath();
+
+  const fileName: string = 'crons.json';
+  const fileContent: object = [];
+
+  await checkCreate(installationPath, fileName, JSON.stringify(fileContent));
+};
