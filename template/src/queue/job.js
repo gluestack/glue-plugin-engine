66858a7fc48f351f41a94380f07e0f3a63d0ed64
Faiz A. Farooqui <faizz.af@gmail.com>
2023-01-16 13:32:07 +0530

    chore: fixed engine's template

--- template/src/queue/job.js
+++ template/src/queue/job.js
@@ -1,6 +1,7 @@
 const Queue = require('./queue').Queue;
 
 const { join } = require('path');
+const { mkdirSync } = require('fs');
 const fsExists = require('../helpers/fs-exists');
 const syncCallsWebhooks = require('../call-manager/sync-webhooks');
 const syncCallsFunctions = require('../call-manager/sync-functions');
@@ -60,7 +61,7 @@ job.runner = (queue) => {
 job.init = () => {
   const filePath = join(__dirname, './../..', '.queue');
   if (!fsExists(filePath)) {
-    fs.mkdirSync(filePath);
+    mkdirSync(filePath);
   }
 
   job.worker = new Queue('.queue', (err) => {
